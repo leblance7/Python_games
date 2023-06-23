3993b18717da884c8511081d4911ce591a56bd1b
Ethan LeBlanc <leblance7@gamil.com>
2023-06-23 14:43:45 -0500

    enemy and stand_stats update
    creating the start of an enemy generator using a fuction and putting stand stats inside a function so the varible becomes local

--- Steel_Ball_Run_rpg.py
+++ Steel_Ball_Run_rpg.py
@@ -1,11 +1,33 @@
-#STEELBALLRUN_rpg
-#To create a text based, stragety rpg game, that allows battles between charcters based
+# STEELBALLRUN_rpg
+# 1) To create a text based, stragety rpg game, that allows battles between charcters based
 # on stats of a character build.
-#6-15-2022 start
-#A function that allows the placement of stats using 
+# 6-15-2022 start
+# 2) A function that allows the placement of stats and uses the same function to
+# to rename enemys the players will fight against
+import random
+
 stand_name = input("Enter the name of your stand: ")
-stand_stats = {"power": 0, "speed": 0, "range": 0, "stamina": 0, "precision": 0,
+##stand = {stand_name: stand_stats}
+
+#shifting stand_stats into a function so the varible becomes local
+def stand_stats(name: str) -> dict:
+    stand_stats = {"power": 0, "speed": 0, "range": 0, "stamina": 0, "precision": 0,
                "potential": 0, "vitality": 0}
+    return stand_stats
+
+
+def enemies():
+    enemies = ["strong", "fast", "tough"]
+    for x in enemies:
+        encounter = random.choice(enemies)
+    stats = stand_stats(stand_name)
+    enemy_stand = {encounter: stand_stats}
+    return enemy_stand
+enemies()
+    
+
+##print(stand)
+
 level = 5
 
 
@@ -15,13 +37,15 @@ def level_up():
     global level
     print("Please enter the the stat you would like to increase.")
 
-#creating the for loop to determine which stats your points go into,
-#resets loop with corrected number to distrubte points and reuturn the updted dictionary
+# creating the for loop to determine which stats your points go into,
+# resets loop with corrected number to distrubte points and reuturn the updated dictionary
 
     for x in range(level):
         skill = input("Choose from power, p, speed, s, range, r, stamina, st,\n" +
                   "precision, pr, potential, po, or vitality, v: ").lower()
-        # Case statements
+
+# conditional statements of where points will go based on skill input
+
         print(skill)
         if skill in ["p", "power"]: 
             stand_stats["power"] += 1
@@ -54,7 +78,15 @@ def level_up():
 #creating while loop to add lost entries to stats
 def point_placement():
     while level > 0:
-        print(level_up())
+        level_up()
 
 point_placement()
-
+ 
+# creating new stand enentries for players to fight     
+purple_hermit = "purple hermit" 
+stand_purple = {purple_hermit: stand_stats}
+print(stand_purple)
+##for x in stand:
+##    print(x)
+##    for x in stand["jojo"]:
+##        print(x, stand_stats[x])
